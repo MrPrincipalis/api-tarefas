fb580337c57254e9c3edf5a418f19c4c91e0e84b
MrPrincipalis <luisfelipevelosoguimaraes@gmail.com>
2025-10-19 21:36:14 -0300

    Create TarefaRepository.java

---
+++ src/main/java/com/example/tarefa_api/repository/TarefaRepository.java
@@ -0,0 +1,6 @@
+package com.example.tarefa_api.repository;
+
+import com.example.tarefa_api.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {}
